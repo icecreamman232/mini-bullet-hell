566209fe0b245ee3ceaeb32c033a4312b1b0624d
Stephen Ho <stephen.ho.232@gmail.com>
2023-08-23 00:28:27 +0700

    Add derbis collect event

--- Assets/_Scripts/Enemy/Derbis.cs
+++ Assets/_Scripts/Enemy/Derbis.cs
@@ -1,3 +1,4 @@
+using JustGame.Scripts.ScriptableEvent;
 using UnityEngine;
 
 namespace JustGame.Scripts.Items
@@ -5,10 +6,17 @@ namespace JustGame.Scripts.Items
     public class Derbis : Item
     {
         [SerializeField] private float m_liveDuration;
-
+        [SerializeField] private IntEvent m_collectEvent;
+        
         private void OnEnable()
         {
             Invoke(nameof(OnSetDestroy),m_liveDuration);
+        }
+
+        protected override void OnSetDestroy()
+        {
+            m_collectEvent.Raise(1);
+            base.OnSetDestroy();
         }
     }
 }
