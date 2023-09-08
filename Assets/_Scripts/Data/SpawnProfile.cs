b7e90ee228903bec715de3b4b44e7ccebb2f09ef
Stephen Ho <stephen.ho.232@gmail.com>
2023-09-08 18:11:24 +0700

    Add special spawns

--- Assets/_Scripts/Data/SpawnProfile.cs
+++ Assets/_Scripts/Data/SpawnProfile.cs
@@ -7,15 +7,45 @@ namespace JustGame.Scripts.Data
     public class SpawnProfile : ScriptableObject
     {
         public float DelayTimeBetweenTwoSpawn;
+
+        public int AmountEnemyBeforeSpawnSpecial;
+        
+        [Header("Normal")]
         [SerializeField] private WeightObject[] m_weights;
         [SerializeField] private GameObject[] m_spawnArray;
 
+        [Header("Special enemy")] 
+        [SerializeField] private WeightObject[] m_specialWeight;
+        [SerializeField] private GameObject[] m_specialEnemies;
+
+        private int m_spawnedEnemyBeforeSpecial;
+        
         private void OnEnable()
         {
             WeightObject.ComputeSpawnArray(m_weights);
         }
 
         public GameObject GetNextSpawn()
+        {
+            m_spawnedEnemyBeforeSpecial++;
+            if (m_spawnedEnemyBeforeSpecial >= AmountEnemyBeforeSpawnSpecial)
+            {
+                m_spawnedEnemyBeforeSpecial = 0;
+                return GetSpecialSpawn();
+            }
+            else
+            {
+                return GetNormalSpawn();
+            }
+        }
+
+        private GameObject GetSpecialSpawn()
+        {
+            var random = Random.Range(0f, 100f);
+            var index = WeightObject.GetWeightIndex(m_specialWeight, random);
+            return m_specialEnemies[index];
+        }
+        private GameObject GetNormalSpawn()
         {
             var random = Random.Range(0f, 100f);
             var index = WeightObject.GetWeightIndex(m_weights, random);
