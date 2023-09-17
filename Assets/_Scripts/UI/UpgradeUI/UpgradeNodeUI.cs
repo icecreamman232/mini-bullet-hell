9f6bb29eae82950920559acebb1ada06b0d7d20e
Stephen Ho <stephen.ho.232@gmail.com>
2023-09-17 20:15:13 +0700

    Rework layout

--- Assets/_Scripts/UI/UpgradeUI/UpgradeNodeUI.cs
+++ Assets/_Scripts/UI/UpgradeUI/UpgradeNodeUI.cs
@@ -1,8 +1,5 @@
-using System;
 using JustGame.Scripts.Data;
-using JustGame.Scripts.Managers;
 using JustGame.Scripts.ScriptableEvent;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -12,12 +9,14 @@ namespace JustGame.Scripts.UI
     public class UpgradeNodeUI : Selectable
     {
         [Header("UI")]
-        [SerializeField] private Image m_icon;
+        [SerializeField] private Image m_iconBG;
         [SerializeField] private Image m_progressBar;
         [Header("Upgrade")] 
         [SerializeField] private ResourceEvent m_resourceEvent;
         [SerializeField] private UpgradeProfile m_upgradeProfile;
-
+        [Header("Branches")] 
+        [SerializeField] private GameObject[] m_branches;
+        
         private readonly float m_delayBetweenTwoSpending = 0.2f;
         private float m_timer;
         private bool m_isPressingDown;
@@ -29,33 +28,47 @@ namespace JustGame.Scripts.UI
         
         private void Show()
         {
-            m_icon.raycastTarget = true;
+            m_iconBG.raycastTarget = true;
         }
 
         private void Hide()
         {
-            m_icon.raycastTarget = false;
+            m_iconBG.raycastTarget = false;
         }
 
+        private void OnUnlockOtherNodes()
+        {
+            for (int i = 0; i < m_branches.Length; i++)
+            {
+                m_branches[i].gameObject.SetActive(true);
+            }
+        }
+        
         private void Update()
         {
+            if (m_upgradeProfile == null) return;
+            if (m_upgradeProfile.IsUnlocked) return;
             if(!m_isPressingDown) return;
 
             m_timer += Time.unscaledDeltaTime;
             if (m_timer >= m_delayBetweenTwoSpending)
             {
                 m_timer = 0;
-                if (m_upgradeProfile == null) return;
                 if (!m_resourceEvent.CanSpend) return;
                 m_resourceEvent.SpendDerbis(1);
                 m_upgradeProfile.SpentXP(1);
                 m_progressBar.fillAmount = ((float)(m_upgradeProfile.CurrentXP)) / m_upgradeProfile.XPToUnlock;
+                if (m_upgradeProfile.IsUnlocked)
+                {
+                    OnUnlockOtherNodes();
+                }
             }
         }
 
 
         public override void OnPointerDown(PointerEventData eventData)
         {
+            if (m_upgradeProfile.IsUnlocked) return;
             if (m_isPressingDown) return;
             m_isPressingDown = true;
             base.OnPointerDown(eventData);
@@ -63,8 +76,8 @@ namespace JustGame.Scripts.UI
 
         public override void OnPointerUp(PointerEventData eventData)
         {
+            if (m_upgradeProfile.IsUnlocked) return;
             if (!m_isPressingDown) return;
-            
             m_isPressingDown = false;
             base.OnPointerUp(eventData);
         }
