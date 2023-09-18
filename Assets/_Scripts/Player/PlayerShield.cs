2b430111a297d40059b2009eefb05321011a6bb2
Stephen Ho <stephen.ho.232@gmail.com>
2023-09-18 11:42:50 +0700

    Add shaking on shield hit

--- Assets/_Scripts/Player/PlayerShield.cs
+++ Assets/_Scripts/Player/PlayerShield.cs
@@ -1,6 +1,8 @@
 using System;
+using JustGame.Scripts.Data;
 using JustGame.Scripts.Enemy;
 using JustGame.Scripts.Managers;
+using JustGame.Scripts.ScriptableEvent;
 using JustGame.Scripts.Weapons;
 using UnityEngine;
 
@@ -11,6 +13,8 @@ namespace JustGame.Scripts.Player
         [SerializeField] private float m_knockBackForce;
         [SerializeField] private float m_knockBackDuration;
         [SerializeField] private LayerMask m_targetLayerMask;
+        [SerializeField] private ScreenShakeEvent m_screenShakeEvent;
+        [SerializeField] private ShakeProfile m_shakeProfile;
 
         public Action OnShieldHit;
         
@@ -38,6 +42,8 @@ namespace JustGame.Scripts.Player
             var enemyMovement = other.gameObject.GetComponent<EnemyMovement>();
             var knockBackDir = (other.transform.position - transform.position).normalized; 
             enemyMovement.ApplyKnockBack(knockBackDir,m_knockBackForce, m_knockBackDuration);
+            
+            m_screenShakeEvent.DoShake(m_shakeProfile);
             OnShieldHit?.Invoke();
         }
 
@@ -45,6 +51,8 @@ namespace JustGame.Scripts.Player
         {
             var projectile = other.gameObject.GetComponentInParent<Projectile>();
             projectile.DestroyBullet(null);
+            
+            m_screenShakeEvent.DoShake(m_shakeProfile);
             OnShieldHit?.Invoke();
         }
     }
