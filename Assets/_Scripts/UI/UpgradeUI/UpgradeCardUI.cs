4963eecf0c0fb1a7b801a58b321b2e3d9b7d42dc
Stephen Ho <stephen.ho.232@gmail.com>
2024-03-10 22:22:26 +0700

    Upgrade color of upgrade card

--- Assets/_Scripts/UI/UpgradeUI/UpgradeCardUI.cs
+++ Assets/_Scripts/UI/UpgradeUI/UpgradeCardUI.cs
@@ -8,6 +8,13 @@ using UnityEngine.UI;
 
 namespace JustGame.Scripts.UI
 {
+    [Serializable]
+    public struct RankColor
+    {
+        public UpgradeRank Rank;
+        public Color Foreground;
+        public Color Background;
+    }
     public class UpgradeCardUI : Selectable
     {
         [SerializeField] [ReadOnly] private AttributeUpgradeBase m_upgradeData;
@@ -15,8 +22,13 @@ namespace JustGame.Scripts.UI
         [Space] 
         [SerializeField] private Image m_outline;
         [SerializeField] private Image m_icon;
+        [SerializeField] private Image m_background;
+        [SerializeField] private Image m_foreground;
         [SerializeField] private TextMeshProUGUI m_nameTxt;
         [SerializeField] private TextMeshProUGUI m_descTxt;
+
+        [Space] [Header("Rank Color Code")] 
+        [SerializeField] private RankColor[] m_rankColors;
         
         public Action<UpgradeCardUI> OnSelectAction;
 
@@ -31,6 +43,23 @@ namespace JustGame.Scripts.UI
         {
             m_upgradeData = data;
             m_nameTxt.text = m_upgradeData.UpgradeName;
+
+            switch (data.Rank)
+            {
+                case UpgradeRank.BRONZE:
+                    m_background.color = m_rankColors[0].Background;
+                    m_foreground.color = m_rankColors[0].Foreground;
+                    break;
+                case UpgradeRank.SILVER:
+                    m_background.color = m_rankColors[1].Background;
+                    m_foreground.color = m_rankColors[1].Foreground;
+                    break;
+                case UpgradeRank.GOLD:
+                    m_background.color = m_rankColors[2].Background;
+                    m_foreground.color = m_rankColors[2].Foreground;
+                    break;
+            }
+            
         }
 
         public override void OnSelect(BaseEventData eventData)
